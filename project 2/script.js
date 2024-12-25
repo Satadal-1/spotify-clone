c894a288eb216e7cc7b011d8c126198cf29f315a
Satadal-1 <satadalhalder04@gmail.com>
2024-12-25 14:24:05 +0530

    updated

--- project 2/script.js
+++ project 2/script.js
@@ -20,9 +20,22 @@ async function getSongs() {
 async function main() {
   let songs = await getSongs()
   console.log(songs);
-  let songUL =document.querySelector(".songlist").getElementsByTagName("ul")[0]
+  let songUL =document.querySelector(".songList").getElementsByTagName("ul")[0]
   for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20"," ")}</li>`;
+    songUL.innerHTML = songUL.innerHTML + `<li>
+    
+                <img class="invert" width="34" src="music.svg" alt="">
+                <div class="info">
+                    <div> ${song.replaceAll("%20"," ")}</div>
+                    <div>Harry</div>
+                </div>
+                <div class="playnow">
+                    <span>Play Now</span>
+                    <img class="invert" src="playbar.svg" alt="">
+                </div>
+               
+    
+    </li>`;
   }
   //play the first song
   var audio = new Audio(songs[0]);
